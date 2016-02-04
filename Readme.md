09ad62cf588ee5fda9c1eaab69d892adc97894c8
pacozaa <pacosarin@gmail.com>
2016-02-04 23:32:27 +0700

    edit for server side

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
 #Caman JS - Canvas Image Manipulation
-This is a Meteor Wrapper from Caman JS. Its only exposing to the Client Side.
+This is a Meteor Wrapper from Caman JS.
 
 Full Docs at: [Caman Homepage](http://camanjs.com/)
--- package.js
+++ package.js
@@ -2,11 +2,11 @@ Package.describe({
   name: 'nerdmed:camanjs',
   summary: 'CamanJs - canvas manipulation in Javascript / client side only',
   version: '0.0.1',
-  git: 'https://github.com/nerdmed/meteor-camanjs.git'
+  git: 'https://github.com/pacozaa/meteor-camanjs.git'
 });
 
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
-  api.addFiles('caman.full.js','client');
+  api.addFiles('caman.full.js');
 });
